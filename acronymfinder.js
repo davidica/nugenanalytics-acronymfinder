2c20b5ac390f9c2827ab860707b93d5a2edc3df9
Davida Trumbo <dptrumbo@gmail.com>
2026-08-17 06:01:38 -0400

    Update acronym finder and acronym definitions

--- acronymfinder.js
+++ acronymfinder.js
@@ -17,6 +17,205 @@ const elements = {
     empty: $('empty'),
     theme: $('theme')
 };
+ 
+const sourceUrls = {
+
+    /* ============================================================
+       CMMC / eCFR
+       ============================================================ */
+
+    "32 CFR Part 170":
+        "https://www.ecfr.gov/current/title-32/subtitle-A/chapter-I/subchapter-D/part-170",
+
+    "32 CFR 170.4 / 170.10":
+        "https://www.ecfr.gov/current/title-32/subtitle-A/chapter-I/subchapter-D/part-170",
+
+    "32 CFR 170.12":
+        "https://www.ecfr.gov/current/title-32/subtitle-A/chapter-I/subchapter-D/part-170/section-170.12",
+
+    "32 CFR 170.14(c)(1)":
+        "https://www.ecfr.gov/current/title-32/subtitle-A/chapter-I/subchapter-D/part-170/section-170.14",
+
+    "32 CFR 170.24":
+        "https://www.ecfr.gov/current/title-32/subtitle-A/chapter-I/subchapter-D/part-170/section-170.24",
+
+    "32 CFR Part 170 / DFARS":
+        "https://www.ecfr.gov/current/title-32/subtitle-A/chapter-I/subchapter-D/part-170",
+
+    "CMMC Program":
+        "https://dodcio.defense.gov/CMMC/Resources-Documentation/",
+
+    "CMMC Program terminology":
+        "https://dodcio.defense.gov/CMMC/Resources-Documentation/",
+
+    "CMMC Program / NIST":
+        "https://dodcio.defense.gov/CMMC/Resources-Documentation/",
+
+
+    /* ============================================================
+       CUI / NARA
+       ============================================================ */
+
+    "32 CFR 2002.4(h) / NIST":
+        "https://csrc.nist.gov/glossary/term/controlled_unclassified_information",
+
+    "NARA CUI Program":
+        "https://www.archives.gov/cui",
+
+
+    /* ============================================================
+       NIST
+       ============================================================ */
+
+    "NIST":
+        "https://www.nist.gov/",
+
+    "NIST CSRC Glossary":
+        "https://csrc.nist.gov/glossary",
+
+    "NIST SP 800-171 / CMMC":
+        "https://csrc.nist.gov/pubs/sp/800/171/r2/upd1/final",
+
+    "NIST SP 800-171 Rev. 2 / CMMC":
+        "https://csrc.nist.gov/pubs/sp/800/171/r2/upd1/final",
+
+    "NIST SP 800-171 Rev. 3 / NIST CSRC Glossary":
+        "https://csrc.nist.gov/pubs/sp/800/171/r3/final",
+
+    "NIST SP 800-39 / NIST CSRC Glossary":
+        "https://csrc.nist.gov/pubs/sp/800/39/final",
+
+    "NIST SP 800-82":
+        "https://csrc.nist.gov/pubs/sp/800/82/r3/final",
+
+    "NIST SP 800-82 Rev. 3 / NIST CSRC Glossary":
+        "https://csrc.nist.gov/pubs/sp/800/82/r3/final",
+
+    "NIST / Industrial Cybersecurity terminology":
+        "https://csrc.nist.gov/pubs/sp/800/82/r3/final",
+
+    "NIST CSRC Glossary / NIST SP 800-92":
+        "https://csrc.nist.gov/glossary",
+
+    "FedRAMP / NIST":
+        "https://www.fedramp.gov/",
+
+
+    /* ============================================================
+       FAR / Acquisition
+       ============================================================ */
+
+    "FAR":
+        "https://www.acquisition.gov/far",
+
+    "FAR 1.101":
+        "https://www.acquisition.gov/far/1.101",
+
+    "FAR 4.1901 / CMMC":
+        "https://www.acquisition.gov/far/4.1901",
+
+    "FAR 52.204-16":
+        "https://www.acquisition.gov/far/52.204-16",
+
+    "FAR / SAM.gov":
+        "https://sam.gov/",
+
+    "Federal Acquisition terminology":
+        "https://www.acquisition.gov/far",
+
+
+    /* ============================================================
+       DFARS
+       ============================================================ */
+
+    "DFARS":
+        "https://www.acquisition.gov/dfars",
+
+
+    /* ============================================================
+       FedRAMP
+       ============================================================ */
+
+    "FedRAMP":
+        "https://www.fedramp.gov/",
+
+
+    /* ============================================================
+       Department of Defense / DCMA
+       ============================================================ */
+
+    "Department of Defense":
+        "https://www.defense.gov/",
+
+    "U.S. Department of Defense":
+        "https://www.defense.gov/",
+
+    "Defense Contract Management Agency":
+        "https://www.dcma.mil/",
+
+    "DCMA DIBCAC":
+        "https://www.dcma.mil/DIBCAC/",
+
+    "Department of Defense PIEE":
+        "https://piee.eb.mil/",
+
+
+    /* ============================================================
+       Federal Systems / Government
+       ============================================================ */
+
+    "Code of Federal Regulations":
+        "https://www.ecfr.gov/",
+
+    "Federal IT terminology":
+        "https://www.cio.gov/",
+
+    "Federal Information Technology terminology":
+        "https://www.cio.gov/",
+
+    "Federal statistical and acquisition terminology":
+        "https://www.census.gov/naics/",
+
+    "Paperwork Reduction Act":
+        "https://www.govinfo.gov/content/pkg/USCODE-2023-title44/html/USCODE-2023-title44-chap35.htm",
+
+
+    /* ============================================================
+       SAM
+       ============================================================ */
+
+    "SAM.gov":
+        "https://sam.gov/",
+
+
+    /* ============================================================
+       Standards Organizations
+       ============================================================ */
+
+    "IEC":
+        "https://www.iec.ch/",
+
+    "ISO / IEC":
+        "https://www.iso.org/",
+
+
+    /* ============================================================
+       General Cybersecurity
+       ============================================================ */
+
+    "Cybersecurity industry terminology":
+        "https://csrc.nist.gov/glossary"
+};
+
+ 
+ 
+function getSourceUrl(item) {
+    if (item.sourceUrl) {
+        return item.sourceUrl;
+    }
+
+    return sourceUrls[item.source] || null;
+}
 
 function escapeHtml(value) {
     return String(value).replace(
@@ -93,16 +292,32 @@ function getFilteredItems() {
                         </p>
                     `
                     : '';
+             
+            const sourceUrl =
+                getSourceUrl(item);
 
-            const source =
-                item.source
-                    ? `
-                        <div class="source">
-                            <strong>Source:</strong>
-                            ${escapeHtml(item.source)}
-                        </div>
-                    `
-                    : '';
+             const source =
+    item.source
+        ? `
+            <div class="source">
+                <strong>Source:</strong>
+                ${
+                    sourceUrl
+                        ? `
+                            <a
+                                href="${escapeHtml(sourceUrl)}"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                ${escapeHtml(item.source)}
+                            </a>
+                        `
+                        : escapeHtml(item.source)
+                }
+            </div>
+        `
+        : '';
+        
 
             const sourceType =
                 item.sourceType
